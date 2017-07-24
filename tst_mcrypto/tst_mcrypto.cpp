a61adc62da8b47e5d369fba5cb0a5a66e0631103
Emil Sawicki <esawicki@milosolutions.com>
2017-07-24 13:47:11 +0200

    Unit tests

---
+++ tst_mcrypto/tst_mcrypto.cpp
@@ -0,0 +1,80 @@
+/*******************************************************************************
+Copyright (C) 2016 Milo Solutions
+Contact: https://www.milosolutions.com
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*******************************************************************************/
+
+#include <QtTest>
+#include <QCoreApplication>
+
+#include "mcrypto.h"
+
+class TestMCrypto : public QObject
+{
+   Q_OBJECT
+
+private slots:
+    void initTestCase();
+    void cleanupTestCase();
+
+    void testEncrypted();
+    void testEncryptedStatic();
+};
+
+void TestMCrypto::initTestCase()
+{
+    QCoreApplication::setApplicationName("MCrypto Test");
+    QCoreApplication::setOrganizationName("Milo");
+}
+
+void TestMCrypto::cleanupTestCase()
+{
+}
+
+void TestMCrypto::testEncrypted()
+{
+    const QByteArray pass(TestMCrypto::metaObject()->className());
+    const QByteArray data("The Advanced Encryption Standard (AES).");
+    QByteArray rawData = data;
+
+    MCrypto crypt(MCrypto::AES_256, MCrypto::CBC);
+    QByteArray encryptedData = crypt.encrypt(rawData, pass);
+
+    MCrypto crypt2(MCrypto::AES_256, MCrypto::CBC);
+    QByteArray decryptedData = crypt2.decrypt(encryptedData, pass);
+
+    QCOMPARE(data, decryptedData);
+}
+
+void TestMCrypto::testEncryptedStatic()
+{
+    const QByteArray pass(TestMCrypto::metaObject()->className());
+    const QByteArray data("The Advanced Encryption Standard (AES).");
+    QByteArray rawData = data;
+
+    QByteArray encryptedData = MCrypto::encrypt(MCrypto::AES_256, MCrypto::CBC, rawData, pass);
+    QByteArray decryptedData = MCrypto::decrypt(MCrypto::AES_256, MCrypto::CBC, encryptedData, pass);
+
+    QCOMPARE(data, decryptedData);
+}
+
+QTEST_MAIN(TestMCrypto)
+
+#include "tst_mcrypto.moc"
